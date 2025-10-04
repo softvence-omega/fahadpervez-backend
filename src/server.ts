cadc1c35611a78ba63a584b1611750800775e0b3
abumahid <eng.marufbilla@gmail.com>
2025-10-04 11:22:22 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { makeDefaultAdmin } from "./app/utils/makeDefaultAdmin";
 async function main() {
     await mongoose.connect(configs.db_url!);
     await makeDefaultAdmin()
-    app.listen(configs.port, () => {
+    app.listen(Number(configs.port), '0.0.0.0', () => {
         console.log(`Server listening on port ${configs.port}`);
     });
 }
