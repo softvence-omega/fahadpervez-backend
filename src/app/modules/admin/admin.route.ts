2dcabbe23d76b0fcaa6af3a8f8196df4e43eb721
abumahid <eng.marufbilla@gmail.com>
2025-10-09 12:26:14 +0600

    update admin overview

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -5,9 +5,21 @@ import { admin_controller } from "./admin.controller";
 const adminRouter = Router();
 
 adminRouter.get("/overview", auth("ADMIN"), admin_controller.get_all_overview_data)
+
+// for student
 adminRouter.get("/students", auth("ADMIN"), admin_controller.get_all_student)
 adminRouter.get("/student/:studentId", auth("ADMIN"), admin_controller.get_single_student)
 adminRouter.delete("/student/:studentId", auth("ADMIN"), admin_controller.delete_student)
+
+// for professional
+adminRouter.get("/professionals", auth("ADMIN"), admin_controller.get_all_professional)
+adminRouter.get("/professional/:accountId", auth("ADMIN"), admin_controller.get_single_professional)
+adminRouter.delete("/professional/:accountId", auth("ADMIN"), admin_controller.delete_professional)
+
+// for mentor
+adminRouter.get("/mentors", auth("ADMIN"), admin_controller.get_all_mentor)
+adminRouter.get("/mentor/:accountId", auth("ADMIN"), admin_controller.get_single_mentor)
+adminRouter.delete("/mentor/:accountId", auth("ADMIN"), admin_controller.delete_mentor)
 
 
 
