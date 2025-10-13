8d2d1c7e79917b128f207f110042dbb074af2ab2
abumahid <eng.marufbilla@gmail.com>
2025-10-13 09:52:20 +0600

    update app and swagger

---
+++ src/swaggerOptions.ts
@@ -0,0 +1,55 @@
+import path from "path";
+import { configs } from "./app/configs";
+import { adminSwaggerDoc } from "./app/modules/admin/admin.swagger";
+import { authDocs } from "./app/modules/auth/auth.swagger";
+import { chatSwaggerDoc } from "./app/modules/chat/chat.swagger";
+import { clinicalCaseSwagger } from "./app/modules/clinical_case/clinical_case.swagger";
+import { mcqBankSwaggerDoc } from "./app/modules/mcq_bank/mcq_bank.swagger";
+import { socialPostDocs } from "./app/modules/social_post/social_post.swagger";
+
+export const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Fahad Pervez API - Team Future-Stack",
+            version: "1.0.0",
+            description: "Express API with auto-generated Swagger docs",
+        },
+        paths: {
+            ...authDocs,
+            ...adminSwaggerDoc,
+            ...clinicalCaseSwagger,
+            ...socialPostDocs,
+            ...chatSwaggerDoc,
+            ...mcqBankSwaggerDoc,
+        },
+        servers: configs.env === "production" ? [
+            { url: "https://fahadpervez-backend.onrender.com" },
+            { url: "http://localhost:5000" },
+        ] : [
+            { url: "http://localhost:5000" },
+            { url: "https://fahadpervez-backend.onrender.com" },
+        ],
+        components: {
+            securitySchemes: {
+                AuthorizationToken: {
+                    type: "apiKey",
+                    in: "header",
+                    name: "Authorization",
+                    description: "Put your accessToken here ",
+                },
+            },
+        },
+        security: [
+            {
+                AuthorizationToken: []
+            },
+        ],
+    },
+    apis: [
+        path.join(
+            __dirname,
+            configs.env === "production" ? "./**/*.js" : "./**/*.ts"
+        ),
+    ],
+};
